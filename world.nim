55cf6764c1102282abcf721f83d53ae8c14f6e12
Pavel Anossov <anossov@gmail.com>
2015-10-12 23:56:50 +0300

    Fix world

--- world.nim
+++ world.nim
@@ -1,7 +1,9 @@
+import logging
 import renderer
 import cube
 import mesh
 import gl/texture
+import objfile
 
 type
   World = ref object
@@ -11,16 +13,21 @@ type
 
 
 proc newWorld*(r: RenderSystem): World =
+  info("World init start")
+  var data = loadObj("assets/test.obj")
+
   result = World(
     R: r,
-    cube: newMesh(cubeVertices, newTexture("assets/test.png")),
+    cube: newMesh(data, newTexture("assets/test.png")),
     cubes: newSeq[Transform](),
   )
-
+ 
   result.R.view = newTransform([0.0'f32, 0.0, 2.0], [0.0'f32, 0.0, 0.0], [1.0'f32, 1.0, 1.0])
 
-  result.cubes.add(newTransform([0.0'f32, 0.0, 0.0]))
-  result.cubes.add(newTransform([-1.0'f32, 0.0, 0.0], zero, [0.3'f32, 0.3, 0.3]))
+  result.cubes.add(newTransform([0.0'f32, 0.0, 0.0], zero, [0.3'f32, 0.3, 0.3]))
+  result.cubes.add(newTransform([-1.0'f32, 0.0, 0.0], zero, [0.1'f32, 0.1, 0.1]))
+
+  info("World init end")
 
 proc update*(w: World; time, delta: float) =
   for i, c in mpairs(w.cubes):
